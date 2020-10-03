6876104f80e8fa4587571fd313de9ff75b2d6ad3
unknown <erler.etienne@gmail.com>
2020-10-03 19:46:28 +0200

    asdf

--- code/main.cpp
+++ code/main.cpp
@@ -4,6 +4,7 @@
 
 #include "map.hpp"
 #include "typedef.hpp"
+#include "const.hpp"
 
 //all structure headers
 #include "structures/base.hpp"
@@ -22,15 +23,10 @@ int main() {
     create_miner(t_koords(0, 0), map, STRUCT::coal_miner, ore_miner);
 
     do {
-        run_model(map);
+        run_map_view(map);
     } while (MAIN_LOOP);
 }
 
-void run_model(Map* map){
+void run_map_view(Map* map){
     std::cout << map << std::endl;
-}
-
-void create_miner(t_koords koords, Map* map, STRUCT structure, std::vector <OreMiner*>* ore_miner){
-    OreMiner*   miner   = new OreMiner(koords, map, structure);
-    ore_miner->push_back(miner);
 }
